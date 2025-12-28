6e5173c88e66b0484f2e5a3f477452b05a512b72
mihailgaberov <mihail.gaberov@gmail.com>
2025-12-28 14:30:54 +0100

    :sparkles: implement basic game state and event handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,35 @@
 module Main where
 
-greet :: String -> String
-greet name =
-  "Hello, " ++ name ++ "! Welcome to Haskell."
+-- The state of our game
+data GameState
+  = NotStarted
+  | Playing {secret :: Int, attempts :: Int}
+  | Won {secret :: Int, attempts :: Int}
+  deriving (Show)
+
+-- Events that can happen in the game
+data GameEvent
+  = StartGame Int -- secret number
+  | Guess Int -- player's guess
+  deriving (Show)
+
+applyEvent :: GameState -> GameEvent -> GameState
+applyEvent NotStarted (StartGame secret) =
+  Playing {secret = secret, attempts = 0}
+applyEvent (Playing secret attempts) (Guess n)
+  | n == secret = Won {secret = secret, attempts = attempts + 1}
+  | otherwise = Playing {secret = secret, attempts = attempts + 1}
+-- Ignore invalid events (no state change)
+applyEvent state _ = state
 
 main :: IO ()
 main = do
-  let message = greet "George"
-  putStrLn message
+  let s0 = NotStarted
+  let s1 = applyEvent s0 (StartGame 42)
+  let s2 = applyEvent s1 (Guess 10)
+  let s3 = applyEvent s2 (Guess 42)
+
+  print s0
+  print s1
+  print s2
+  print s3
