ff10e73807adda1b3e2c4480082679ade430aacf
mihailgaberov <mihail.gaberov@gmail.com>
2025-12-28 14:47:58 +0100

    :sparkles: add random number generation and improve game state with hints

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main where
 
+import System.Random (randomRIO)
+
 -- The state of our game
 data GameState
   = NotStarted
-  | Playing {secret :: Int, attempts :: Int}
-  | Won {secret :: Int, attempts :: Int}
+  | Playing
+      { secret :: Int,
+        attempts :: Int,
+        lastHint :: Maybe String
+      }
+  | Won
+      { secret :: Int,
+        attempts :: Int
+      }
   deriving (Show)
 
 -- Events that can happen in the game
@@ -15,29 +24,46 @@ data GameEvent
 
 applyEvent :: GameState -> GameEvent -> GameState
 applyEvent NotStarted (StartGame secret) =
-  Playing {secret = secret, attempts = 0}
-applyEvent (Playing secret attempts) (Guess n)
-  | n == secret = Won {secret = secret, attempts = attempts + 1}
-  | otherwise = Playing {secret = secret, attempts = attempts + 1}
--- Ignore invalid events (no state change)
+  Playing secret 0 Nothing
+applyEvent (Playing secret attempts _) (Guess n)
+  | n == secret =
+      Won secret (attempts + 1)
+  | n < secret =
+      Playing secret (attempts + 1) (Just "Too low!")
+  | otherwise =
+      Playing secret (attempts + 1) (Just "Too high!")
 applyEvent state _ = state
 
 render :: GameState -> IO ()
 render NotStarted =
   putStrLn "Welcome! Type 'start' to begin."
-render (Playing _ attempts) =
-  putStrLn ("Make a guess! Attempts so far: " ++ show attempts)
+render (Playing _ attempts hint) = do
+  putStrLn ("Attempts so far: " ++ show attempts)
+  case hint of
+    Just h -> putStrLn h
+    Nothing -> pure ()
+  putStrLn "Enter your guess:"
 render (Won _ attempts) =
   putStrLn ("You won in " ++ show attempts ++ " attempts!")
 
+parseInt :: String -> Maybe Int
+parseInt s =
+  case reads s of
+    [(n, "")] -> Just n
+    _ -> Nothing
+
 readEvent :: GameState -> IO GameEvent
 readEvent NotStarted = do
-  _ <- getLine
-  -- for now, fixed secret (we’ll improve this later)
-  pure (StartGame 42)
-readEvent (Playing _ _) = do
+  secret <- randomRIO (1, 100)
+  putStrLn "Game started! Guess a number between 1 and 100."
+  pure (StartGame secret)
+readEvent (Playing _ _ _) = do
   input <- getLine
-  pure (Guess (read input))
+  case parseInt input of
+    Just n -> pure (Guess n)
+    Nothing -> do
+      putStrLn "Please enter a valid number."
+      readEvent (Playing 0 0 Nothing) -- state doesn't matter here
 readEvent (Won _ _) =
   -- no more events once won
   pure (Guess 0)
